358e2d3d9168124e7b56d65b6f98fd64659bb8d1
Elliot D <111321481+eedeb@users.noreply.github.com>
2026-05-13 12:26:44 -0400

    Implement smart TV control functions

---
+++ smart_tv.py
@@ -0,0 +1,70 @@
+import requests
+import json
+
+TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2ZDk2ZWFkOTNmNzM0ODMxYWU1NjM0MWUzODUxYTA2NCIsImlhdCI6MTc3ODU0MzA1OSwiZXhwIjoyMDkzOTAzMDU5fQ.7ghS0EIOx2TqBANnLYjcu4-eYK6eA7ZsqrOOMfq8AuI"
+
+headers = {
+    "Authorization": f"Bearer {TOKEN}",
+    "Content-Type": "application/json"
+}
+def tv_on():
+    url = "https://home.eedeb.dev/api/services/media_player/turn_on"
+
+    data = {
+        "entity_id": "media_player.smart_tv"
+    }
+
+    r = requests.post(url, headers=headers, json=data)
+
+    print(r.status_code)
+    print(r.text)
+def tv_off():
+    url = "https://home.eedeb.dev/api/services/media_player/turn_off"
+
+    data = {
+        "entity_id": "media_player.smart_tv"
+    }
+
+    r = requests.post(url, headers=headers, json=data)
+
+    print(r.status_code)
+    print(r.text)
+def volume_down():
+    url = "https://home.eedeb.dev/api/services/media_player/volume_down"
+
+    data = {
+        "entity_id": "media_player.smart_tv",
+    }
+
+    r = requests.post(url, headers=headers, json=data)
+
+    print(r.status_code)
+    print(r.text)
+def volume_up():
+    url = "https://home.eedeb.dev/api/services/media_player/volume_up"
+
+    data = {
+        "entity_id": "media_player.smart_tv",
+    }
+
+    r = requests.post(url, headers=headers, json=data)
+
+    print(r.status_code)
+    print(r.text)
+    
+def play_youtube(media_id):
+    url = "https://home.eedeb.dev/api/services/media_player/play_media"
+
+    data = {
+        "entity_id": "media_player.google_cast",
+        "media_content_type": "cast",
+        "media_content_id": json.dumps({
+            "app_name": "youtube",
+            "media_id": media_id
+        })
+    }
+
+    r = requests.post(url, headers=headers, json=data)
+
+    print(r.status_code)
+    print(r.text)
