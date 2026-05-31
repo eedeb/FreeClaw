b016c8b51e5d0ae136e04de9bf19568baef4f628
eedeb <111321481+eedeb@users.noreply.github.com>
2026-05-31 19:06:35 -0400

    Update ha_setup.sh

--- ha_setup.sh
+++ ha_setup.sh
@@ -6,3 +6,5 @@ echo "IP saved to .env"
 read -p "Enter the API token for your Home Assistant: " ha_token < /dev/tty
 printf 'HA_TOKEN=%s\n' "$ha_token" >> .env
 echo "Token saved to .env"
+
+sudo systemctl restart FreeClaw.service
